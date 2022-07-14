710f19999a80da57a3e281f4b84dc4cce3460656
andre353 <eva595eva@gmail.com>
2022-07-14 20:59:43 +0300

    Add possible component folder structure: index.js, style.module.css

--- src/components/ComponentFunc/ComponentFunc.jsx
+++ src/components/ComponentFunc/ComponentFunc.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// к каждому классу будет сгенерированно числовой суффикс к назнанию,
+// чтобы стили разных компонентов были разноименными
+import style from './style.module.css';
 
 // если компонент рендерит всегда одно и то же = props не меняются
 // можно обернуть в React.memo для повышения производительности
 // есть риск, когда и если state изменится - рендера точно не будет!
-const ComponentFunc = React.memo((props) => {
+
+// React.memo - функция-обертка, функция высшего порядка, принимающая
+// другую функцию и возвращает нам мемонизированную функцию
+export const ComponentFunc = React.memo((props) => {
   console.log('-------');
   console.warn('ComponentFunc');
-  return <p>{props.funcattr}</p>;
+  return <p className={style.text}>{props.funcattr}</p>;
 });
 
 ComponentFunc.propTypes = {
@@ -25,5 +31,3 @@ ComponentFunc.propTypes = {
 // to the components you use.
 // If a component doesn't have a displayName is will be shown as <Unknown />
 ComponentFunc.displayName = 'ComponentFunc';
-
-export default ComponentFunc;
